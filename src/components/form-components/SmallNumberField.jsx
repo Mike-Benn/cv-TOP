07e01ae53cb719bf888fc4382cdbcc4b00ef9556
Mike-Benn <mbenn49@wgu.edu>
2024-06-24 06:00:20 -0400

    Add placeholder to number field

--- src/components/form-components/SmallNumberField.jsx
+++ src/components/form-components/SmallNumberField.jsx
@@ -1,17 +1,18 @@
 import PropTypes from 'prop-types'
 
 
-function SmallNumberField({ fieldName = "Placeholder" }) {
+function SmallNumberField({ fieldName = "Placeholder", placeHolder = " " }) {
     let forVal = fieldName.split(' ').join('');
 
     return (
         <div className='field'>
-            <label className='field-label' htmlFor={forVal}>{forVal}</label>
+            <label className='field-label' htmlFor={forVal}>{fieldName}</label>
             <input 
                 type="number"
                 name={forVal}
                 id={forVal} 
                 className='text-input'
+                placeholder={placeHolder}
             />
         </div>
     )
@@ -19,6 +20,7 @@ function SmallNumberField({ fieldName = "Placeholder" }) {
 
 SmallNumberField.propTypes = {
     fieldName: PropTypes.string.isRequired,
+    placeHolder: PropTypes.string.isRequired,
 };
 
 export default SmallNumberField
