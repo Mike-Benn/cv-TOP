81b473b308adfe8b58635f5d666d5f456fe168df
Mike-Benn <mbenn49@wgu.edu>
2024-06-24 05:54:56 -0400

    Redo several fields in form Several fields had wrong input type for their data, redid with new fields

--- src/components/form-components/ContactInfoForm.jsx
+++ src/components/form-components/ContactInfoForm.jsx
@@ -1,4 +1,7 @@
 import SmallTextField from './SmallTextField';
+import SmallPhoneNumberField from './SmallPhoneNumberField';
+import SmallEmailField from './SmallEmailField';
+import SmallUrlField from './SmallUrlField';
 
 
 function ContactInfoForm() {
@@ -6,10 +9,10 @@ function ContactInfoForm() {
     return (
         <div className='form-section'>
             <h2 className='form-header'>Contact Info</h2>
-            <SmallTextField fieldName='Email' />
-            <SmallTextField fieldName='Phone Number' />
-            <SmallTextField fieldName='Location' />
-            <SmallTextField fieldName='Website/LinkedIn' />
+            <SmallEmailField fieldName='Email' />
+            <SmallPhoneNumberField fieldName='Phone Number' />
+            <SmallTextField fieldName='Location' placeHolder='Chicago, IL'/>
+            <SmallUrlField fieldName='Website/LinkedIn' />
         </div>
     )
 }
