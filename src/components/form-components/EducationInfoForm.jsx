7b5482f6451c5490251e25788186d8c86c89da39
Mike-Benn <mbenn49@wgu.edu>
2024-06-24 05:56:42 -0400

    Added placeholders for several fields

--- src/components/form-components/EducationInfoForm.jsx
+++ src/components/form-components/EducationInfoForm.jsx
@@ -1,5 +1,4 @@
 import SmallTextField from './SmallTextField';
-import SmallMonthField from './SmallMonthField';
 import SmallNumberField from './SmallNumberField';
 import CheckboxField from './CheckboxField';
 
@@ -10,12 +9,10 @@ function EducationInfoForm() {
     return (
         <div className='form-section'>
             <h2 className='form-header'>Education</h2>
-            <SmallTextField fieldName='University/Organization' />
-            <SmallTextField fieldName='Program/Degree' />
+            <SmallTextField fieldName='University/Organization' placeHolder='University of Illinois' />
+            <SmallTextField fieldName='Program/Degree' placeHolder='BS in Computer Science' />
             <CheckboxField fieldName='Still Enrolled' />
-            <SmallNumberField fieldName='GPA' />
-
-            
+            <SmallNumberField fieldName='GPA (optional)' />
         </div>
     )
 }
