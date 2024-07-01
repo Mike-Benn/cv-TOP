1b3e1f6f7fec5f89333f290a12686ee0d19ef015
Mike-Benn <mbenn49@wgu.edu>
2024-06-30 23:01:37 -0400

    Remove comments and add class to submit button

--- src/components/buttons/SubmitButton.jsx
+++ src/components/buttons/SubmitButton.jsx
@@ -1,30 +1,18 @@
 import PropTypes from 'prop-types'
 
 
-function SubmitButton({ buttonText = "Placeholder", buttonClass = ""}) {
+function SubmitButton({ buttonText = "Placeholder" }) {
     
-
-    /*return (
-        /*<div className='field'>
-            <label className='field-label' htmlFor={forVal}>{fieldName}</label>
-            <input 
-                type="tel"
-                name={forVal}
-                id={forVal} 
-                className='text-input'
-                placeholder='###-###-####'
-            />
-        </div>)*/
-        return (
-            <div className="field">
-                <button type="button" className={buttonClass}>{buttonText}</button>
-            </div>
-        )
+    return (
+        <div className="field">
+            <button type="button" className="submit-btn">{buttonText}</button>
+        </div>
+    )
 }
 
 SubmitButton.propTypes = {
     buttonText: PropTypes.string.isRequired,
-    buttonClass: PropTypes.string.isRequired,
+    
 };
 
 export default SubmitButton
