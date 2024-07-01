20da83194b2cd53e22f34e24b7ae669d11f8b3e5
Mike-Benn <mbenn49@wgu.edu>
2024-07-01 06:31:59 -0400

    Lift a lot of state into parent component

--- src/components/form-components/BulletListField.jsx
+++ src/components/form-components/BulletListField.jsx
@@ -1,50 +1,16 @@
 import PropTypes from 'prop-types';
-import { v4 as uuidv4 } from 'uuid';
-import { useState } from 'react';
-import ListItem from '../lists/ListItem'
+
 import UnorderedList from '../lists/UnorderedList';
 
 
-function BulletListField({ fieldName = "Placeholder" }) {
+function BulletListField({ fieldName = "Placeholder" , onInputChange , onAddChange , currInputVal , itemList  }) {
+    
     let forVal = fieldName.split(' ').join('');
-    const [currVal , setCurrVal] = useState('');
-    const [currList , setCurrList] = useState([]);
     
-
-    const handleChange = (e) => {
-        setCurrVal(e.target.value);
-    }
-
-    const handleSubmit = () => {
-        if (currList.length === 0) {
-            let inputValue = [ {id: uuidv4() , value: currVal} ];
-            setCurrList(inputValue);
-            setCurrVal('');
-        } else if (currList.length > 4) {
-            alert("Please limit your responsibilities to no more than five examples.");
-        } else {
-            let inputValue = [ {id: uuidv4() , value: currVal} ];
-            let updatedList = currList.concat(inputValue);
-            setCurrList(updatedList);
-            setCurrVal('');
-        }
-    }
-
-    const handleClose = (id) => {
-        let updatedList = currList.filter(item => 
-            id !== item.id
-        );
-        setCurrList(updatedList);
-    }
-    
-    const listItems = currList.map(item => 
-        <ListItem key={item.id} id={item.id} text={item.value} onClose={handleClose}  />
-    )
-
     return (
         
         <div className="bullet-field">
-            <UnorderedList itemList={listItems} className="job-experience-list" />
+            <UnorderedList itemList={itemList} className="job-experience-list" />
             <div className="field">
                 <label htmlFor={forVal} className="field-label">{fieldName}</label>
                 <input 
@@ -52,17 +18,26 @@ function BulletListField({ fieldName = "Placeholder" }) {
                     name={forVal}
                     id={forVal}
                     className='text-input'
-                    onChange={handleChange}
-                    value={currVal}
+                    onChange={onInputChange}
+                    value={currInputVal}
                     />
-                <button type="button" className="submit-btn" onClick={handleSubmit}>Add</button>
+                <button type="button" className="submit-btn" onClick={onAddChange}>Add</button>
             </div>
+            
         </div>
+        
     )
 }
 
 BulletListField.propTypes = {
     fieldName: PropTypes.string.isRequired,
+    currInputVal: PropTypes.string,
+    onInputChange: PropTypes.func,
+    onAddChange: PropTypes.func,
+    itemList: PropTypes.array,/* This doesn't feel like it should be an array */
+    
+    
+    
 }
 
 export default BulletListField
