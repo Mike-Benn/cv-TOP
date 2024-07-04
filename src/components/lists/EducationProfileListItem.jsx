29f30d92dfbd777eeaba80b0ae312c17c5ab68ef
Mike-Benn <mbenn49@wgu.edu>
2024-07-04 05:31:43 -0400

    Add list item for education profile

---
+++ src/components/lists/EducationProfileListItem.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types'
+import EditItemButton from '../buttons/EditItemButton'
+import DeleteItemButton from '../buttons/DeleteItemButton'
+
+
+function EducationProfileListItem({ data = {} , onDelete = null , onEdit = null }) {
+    return (
+        <>
+            <li key={data.id}>
+                <p>{`${data.organization} / ${data.program}`}</p>
+                <div className="list-btn-container">
+                    <EditItemButton id={data.id} onEdit={onEdit} />
+                    <DeleteItemButton id={data.id} onDelete={onDelete} />
+                </div>
+            </li>
+        </>
+    )
+}
+
+EducationProfileListItem.propTypes = {
+    data: PropTypes.object.isRequired,
+    onDelete: PropTypes.func,
+    onEdit: PropTypes.func,
+}
+
+export default EducationProfileListItem
