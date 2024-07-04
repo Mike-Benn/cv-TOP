3e1b8d754dcdd641a47ff07f82cd3ddaf79d4e9b
Mike-Benn <mbenn49@wgu.edu>
2024-07-04 05:30:30 -0400

    Add state for onChange and unordered lists

--- src/components/form-components/EducationInfoForm.jsx
+++ src/components/form-components/EducationInfoForm.jsx
@@ -2,19 +2,101 @@ import SmallTextField from './SmallTextField';
 import SmallNumberField from './SmallNumberField';
 import CheckboxField from './CheckboxField';
 import SubmitButton from '../buttons/SubmitButton';
+import EducationProfileListItem from '../lists/EducationProfileListItem';
+import UnorderedList from '../lists/UnorderedList';
+import { v4 as uuidv4 } from 'uuid';
+import { getProfile } from '../../utils/utils';
+import { useState } from 'react';
 
 
 function EducationInfoForm() {
+
+    const [currOrganization , setCurrOrganization] = useState("");
+    const [currProgram , setCurrProgram] = useState("");
+    const [currEnrolled, setCurrEnrolled] = useState(false);
+    const [currGPA , setCurrGPA] = useState("");
+    const [currEducationProfileList , setCurrEducationProfileList] = useState([]);
+    
+
+    const handleOrganizationChange = (e) => {
+        setCurrOrganization(e.target.value);
+    }
+
+    const handleProgramChange = (e) => {
+        setCurrProgram(e.target.value);
+    }
+
+    const handleStillEnrolled = () => {
+        if (currEnrolled) {
+            setCurrEnrolled(false);
+        } else {
+            setCurrEnrolled(true);
+        }
+    }
+
+    const handleGPAChange = (e) => {
+        setCurrGPA(e.target.value);
+    }
+
+    const handleSubmitEducationProfile = () => {
+        if (currEducationProfileList.length <= 3) {
+            let profile = {
+                id: uuidv4(),
+                organization: currOrganization,
+                program: currProgram,
+                stillEnrolled: currEnrolled,
+                gpa: currGPA
+            }
+            let updatedProfileList = currEducationProfileList.concat([profile]);
+            setCurrEducationProfileList(updatedProfileList);
+            resetForm();
+        } else {
+            alert("Please submit no more than three of your most relevant education examples whether they be collegiate degrees or some other form of formal education.");
+        }
+    }
+
+    const handleDeleteEducationProfile = (id) => {
+        let updatedProfList = currEducationProfileList.filter(item =>
+            id !== item.id
+        );
+        setCurrEducationProfileList(updatedProfList);
+    }
+    
+    const handleEditEducationProfile = (id) => {
+        let profileObject = getProfile(id , currEducationProfileList);
+        let profile = profileObject.profile;
+        let educationProfileList = profileObject.arr;
+
+        setCurrOrganization(profile.organization);
+        setCurrProgram(profile.program);
+        setCurrEnrolled(profile.stillEnrolled);
+        setCurrGPA(profile.gpa);
+        setCurrEducationProfileList(educationProfileList);
+
+    }
+    
+    const resetForm = () => {
+        setCurrOrganization("");
+        setCurrProgram("");
+        setCurrEnrolled(false);
+        setCurrGPA("");
+
+    }
+
+    const educationProfileListItems = currEducationProfileList.map(item =>
+        <EducationProfileListItem key={item.id} data={item} onDelete={handleDeleteEducationProfile} onEdit={handleEditEducationProfile} />
+    )
 
 
     return (
         <div className='form-section'>
             <h2 className='form-header'>Education</h2>
-            <SmallTextField fieldName='University/Organization' placeHolder='University of Illinois' />
-            <SmallTextField fieldName='Program/Degree' placeHolder='BS in Computer Science' />
-            <CheckboxField fieldName='Still Enrolled' />
-            <SmallNumberField fieldName='GPA (optional)' />
-            <SubmitButton buttonText='Submit Education Background' />
+            <UnorderedList itemList={educationProfileListItems} />
+            <SmallTextField fieldName='University/Organization' placeHolder='University of Illinois' onInputChange={handleOrganizationChange} value={currOrganization}/>
+            <SmallTextField fieldName='Program/Degree' placeHolder='BS in Computer Science' onInputChange={handleProgramChange} value={currProgram}/>
+            <CheckboxField fieldName='Still Enrolled' onInputChange={handleStillEnrolled} checked={currEnrolled} value={currEnrolled}/>
+            <SmallNumberField fieldName='GPA (optional)' onInputChange={handleGPAChange} value={currGPA}/>
+            <SubmitButton buttonText='Submit Education Background' onClickAction={handleSubmitEducationProfile}/>
         </div>
     )
 }
--- src/components/form-components/TechnicalSkillsForm.jsx
+++ src/components/form-components/TechnicalSkillsForm.jsx
@@ -1,16 +1,102 @@
-import SmallTextField from './SmallTextField';
-import SubmitButton from '../buttons/SubmitButton';
+import { useState } from 'react';
+import BulletListField from './BulletListField';
+import { v4 as uuidv4 } from 'uuid';
+import GeneralListItem from '../lists/GeneralListItem';
+import UnorderedList from '../lists/UnorderedList';
+import { getItemWithID } from '../../utils/utils';
 
 
 function TechnicalSkillsForm() {
 
+    const [currLanguage , setCurrLanguage] = useState("");
+    const [currFrametool , setCurrFrametool] = useState("");
+    const [currLanguageList , setCurrLanguageList] = useState([]);
+    const [currFrametoolList , setCurrFrametoolList] = useState([]);
+
+    const handleLanguageChange = (e) => {
+        setCurrLanguage(e.target.value);
+    }
+
+    const handleFrametoolChange = (e) => {
+        setCurrFrametool(e.target.value);
+    }
+
+    const handleAddCurrLanguage = () => {
+        if (currLanguage.length < 1) {
+            alert("Please list a language.")
+        } else if (currLanguageList.length < 5) {
+            let inputVal = {id: uuidv4() , value: currLanguage };
+            let updatedLanguageList = currLanguageList.concat([inputVal]);
+            setCurrLanguageList(updatedLanguageList);
+            setCurrLanguage("");
+        } else {
+            alert("Please list at max five languages that you feel most comfortable working with.");
+        }
+    }
+
+    const handleAddCurrFrametool = () => {
+        if (currLanguage.length < 1) {
+            alert("Please type a framework or tool here.")
+        } else if (currFrametoolList.length < 5) {
+            let inputVal = {id: uuidv4() , value: currFrametool };
+            let updatedFrametoolList = currFrametoolList.concat([inputVal]);
+            setCurrFrametoolList(updatedFrametoolList);
+            setCurrFrametool("");
+        } else {
+            alert("Please list at max five frameworks, tools, or software ideologies that you feel you are most comfortable working with and represesnts you as a programmer.");
+        }
+    }
+
+    const handleDeleteLanguage = (id) => {
+        let updatedLanguageList = currLanguageList.filter(item =>
+            id !== item.id
+        );
+        setCurrLanguageList(updatedLanguageList);
+    }
+
+    const handleEditLanguage = (id) => {
+        let languageObject = getItemWithID(id , currLanguageList);
+        let language = languageObject.item.value;
+        let languageList = languageObject.arr;
+
+        setCurrLanguage(language);
+        setCurrLanguageList(languageList);
+    }
+
+    const handleDeleteFrametool = (id) => {
+        let updatedFrametoolList = currFrametoolList.filter(item => 
+            id !== item.id
+        );
+        setCurrFrametoolList(updatedFrametoolList);
+    }
+
+    const handleEditFrametool = (id) => {
+        let frametoolObject = getItemWithID(id , currFrametoolList);
+        let frametool = frametoolObject.item.value;
+        let frametoolList = frametoolObject.arr;
+
+        setCurrFrametool(frametool);
+        setCurrFrametoolList(frametoolList);
+    }
+
+    
+
+
+    const languageListItems = currLanguageList.map(item =>
+        <GeneralListItem key={item.id} data={item} onDelete={handleDeleteLanguage} onEdit={handleEditLanguage} />
+    )
+
+    const frametoolListItems = currFrametoolList.map(item => 
+        <GeneralListItem key={item.id} data={item} onDelete={handleDeleteFrametool} onEdit={handleEditFrametool} />
+    )
+
     return (
         <div className='form-section'>
             <h2 className='form-header'>Technical Skills</h2>
-            <SmallTextField fieldName='Programming Languages' />
-            <SubmitButton buttonText='Submit Language' />
-            <SmallTextField fieldName='Frameworks/Tools' />
-            <SubmitButton buttonText='Submit Framework/Tool' />
+            <UnorderedList itemList={languageListItems} />
+            <BulletListField fieldName='Programming Languages' onInputChange={handleLanguageChange} onAddChange={handleAddCurrLanguage} value={currLanguage} />
+            <UnorderedList itemList={frametoolListItems} />
+            <BulletListField fieldName='Frameworks/Tools' onInputChange={handleFrametoolChange} onAddChange={handleAddCurrFrametool} value={currFrametool}/>
         </div>
     )
 }
