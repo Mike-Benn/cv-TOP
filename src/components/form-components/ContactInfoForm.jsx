2a85e109c25dc95a895b11c74a1e459f78be46ef
Mike-Benn <mbenn49@wgu.edu>
2024-07-04 05:28:09 -0400

    Add state setters for fields for onChange triggers

--- src/components/form-components/ContactInfoForm.jsx
+++ src/components/form-components/ContactInfoForm.jsx
@@ -3,16 +3,41 @@ import SmallPhoneNumberField from './SmallPhoneNumberField';
 import SmallEmailField from './SmallEmailField';
 import SmallUrlField from './SmallUrlField';
 
+import { useState } from 'react';
+
 
 function ContactInfoForm() {
+
+
+    const [currEmail , setCurrEmail] = useState("");
+    const [currPhoneNumber , setCurrPhoneNumber] = useState("");
+    const [currLocation , setCurrLocation] = useState("");
+    const [currUrl , setCurrUrl] = useState("");
+    /*const [currContactProfileList, setCurrContactProfileList] = useState("");*/
+
+    const handleEmailChange = (e) => {
+        setCurrEmail(e.target.value);
+    }
+
+    const handlePhoneNumberChange = (e) => {
+        setCurrPhoneNumber(e.target.value);
+    }
+
+    const handleLocationChange = (e) => {
+        setCurrLocation(e.target.value);
+    }
+
+    const handleUrlChange = (e) => {
+        setCurrUrl(e.target.value);
+    }
 
     return (
         <div className='form-section'>
             <h2 className='form-header'>Contact Info</h2>
-            <SmallEmailField fieldName='Email' />
-            <SmallPhoneNumberField fieldName='Phone Number' />
-            <SmallTextField fieldName='Location' placeHolder='Chicago, IL'/>
-            <SmallUrlField fieldName='Website/LinkedIn' />
+            <SmallEmailField fieldName='Email' onInputChange={handleEmailChange} value={currEmail}/>
+            <SmallPhoneNumberField fieldName='Phone Number' onInputChange={handlePhoneNumberChange} value={currPhoneNumber} />
+            <SmallTextField fieldName='Location' placeHolder='Chicago, IL' onInputChange={handleLocationChange} value={currLocation}/>
+            <SmallUrlField fieldName='Website/LinkedIn' onInputChange={handleUrlChange} value={currUrl}/>
         </div>
     )
 }
--- src/components/form-components/PersonalInfoForm.jsx
+++ src/components/form-components/PersonalInfoForm.jsx
@@ -1,16 +1,38 @@
 
 import TextArea from './TextArea';
 import SmallTextField from './SmallTextField';
+import { useState } from 'react';
 
 function PersonalInfoForm() {
+
+    const [currFirstName , setCurrFirstName] = useState("");
+    const [currLastName , setCurrLastName] = useState("");
+    const [currTitle , setCurrTitle] = useState("");
+    const [currSummary , setCurrSummary] = useState("");
+
+    const handleFirstNameChange = (e) => {
+        setCurrFirstName(e.target.value);
+    }
+
+    const handleLastNameChange = (e) => {
+        setCurrLastName(e.target.value);
+    }
+
+    const handleTitleChange = (e) => {
+        setCurrTitle(e.target.value);
+    }
+
+    const handleSummaryChange = (e) => {
+        setCurrSummary(e.target.value);
+    }
 
     return (
         <div className='form-section'>
             <h2 className='form-header'>Personal Info</h2>
-            <SmallTextField fieldName='First Name' />
-            <SmallTextField fieldName='Last Name' />
-            <SmallTextField fieldName='Professional title' />
-            <TextArea fieldName='Give a short summary about yourself' />
+            <SmallTextField fieldName='First Name' onInputChange={handleFirstNameChange} value={currFirstName}/>
+            <SmallTextField fieldName='Last Name' onInputChange={handleLastNameChange} value={currLastName}/>
+            <SmallTextField fieldName='Professional title' onInputChange={handleTitleChange} value={currTitle}/>
+            <TextArea fieldName='Give a short summary about yourself' onInputChange={handleSummaryChange} value={currSummary}/>
         </div>
     )
 }
