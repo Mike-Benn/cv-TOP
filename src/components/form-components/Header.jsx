bccbb51c3494b31efd68c32d1dd5ec531ac7f970
Mike-Benn <mbenn49@wgu.edu>
2024-07-08 07:16:09 -0400

    Add class to header tag

--- src/components/form-components/Header.jsx
+++ src/components/form-components/Header.jsx
@@ -1,9 +1,9 @@
-import PropTypes from 'prop-types'
+
 
 
 function Header() {
     return (
-        <header>
+        <header className='generator-page-header'>
             <h1>Resume Generator</h1>
             <p>Create your resume by filling out the forms below.  Your resume will be updated dynamically in the preview screen as you begin filling in your information.</p>
             <a href="https://github.com/Mike-Benn/cv-TOP" target='_blank'>Check out my project&#39;s repo here!</a>
