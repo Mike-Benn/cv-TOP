7c2b76d6bd36bfbf5c98e5136f6fafaf19300a9a
Mike-Benn <mbenn49@wgu.edu>
2024-07-09 02:55:03 -0400

    Add list components to project profile

--- src/components/preview-components/ProjectProfile.jsx
+++ src/components/preview-components/ProjectProfile.jsx
@@ -1,14 +1,28 @@
 import PropTypes from 'prop-types';
-
+import ProfileListItem from '../lists/ProfileListItem';
+import { createStringFromList } from '../../utils/utils';
 
 function ProjectProfile({ item }) {
+
+    console.log(item)
+
+    let projectName = item.name + " | ";
+    let technologyString = createStringFromList(item.technologies);
+
+    let objectives = item.objectives.map(objective => 
+        <ProfileListItem key={objective.id} item={objective} />
+    )
 
     return (
         <div className="preview-project-profile">
             <div className="preview-section-subheader">
-                <p className="preview-topic">{item.name}</p>
+                <p className="preview-topic">{projectName}<span className='preview-technology'>{technologyString}</span></p>
             </div>
+            <ul>
+                {objectives}
+            </ul>
         </div>
+
     )
     
 
