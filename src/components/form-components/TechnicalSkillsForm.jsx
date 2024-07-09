029140abeb00461999599eb9b9f2f548966b329d
Mike-Benn <mbenn49@wgu.edu>
2024-07-09 02:52:25 -0400

    Delete components doing same task

--- src/components/form-components/TechnicalSkillsForm.jsx
+++ src/components/form-components/TechnicalSkillsForm.jsx
@@ -2,13 +2,10 @@
 import BulletListField from './BulletListField';
 import PropTypes from 'prop-types'
 import GeneralListItem from '../lists/GeneralListItem';
-import UnorderedList from '../lists/UnorderedList';
-
-
 
 function TechnicalSkillsForm({ technicalSkillsData }) {
     
-
+    console.log(technicalSkillsData.technicalSkillsValues.currLanguageList)
     const languageListItems = technicalSkillsData.technicalSkillsValues.currLanguageList.map(item =>
         <GeneralListItem key={item.id} data={item} onDelete={technicalSkillsData.technicalSkillsListeners.handleDeleteLanguage} onEdit={technicalSkillsData.technicalSkillsListeners.handleEditLanguage} />
     )
@@ -28,14 +25,10 @@ function TechnicalSkillsForm({ technicalSkillsData }) {
     return (
         <div className='form-section'>
             <h2 className='form-header'>Technical Skills</h2>
-            <UnorderedList itemList={languageListItems} className="preview-skill-list" />
-            <BulletListField fieldName='Programming Languages' onInputChange={technicalSkillsData.technicalSkillsListeners.handleLanguageChange} onAddChange={technicalSkillsData.technicalSkillsListeners.handleAddCurrLanguage} value={technicalSkillsData.technicalSkillsValues.currLanguage} />
-            <UnorderedList itemList={frameworkListItems} className="preview-skill-list" />
-            <BulletListField fieldName='Frameworks' onInputChange={technicalSkillsData.technicalSkillsListeners.handleFrameworkChange} onAddChange={technicalSkillsData.technicalSkillsListeners.handleAddCurrFramework} value={technicalSkillsData.technicalSkillsValues.currFramework}/>
-            <UnorderedList itemList={devToolListItems} className="preview-skill-list" />
-            <BulletListField fieldName='Developer Tools' onInputChange={technicalSkillsData.technicalSkillsListeners.handleDevToolChange} onAddChange={technicalSkillsData.technicalSkillsListeners.handleAddCurrDevTool} value={technicalSkillsData.technicalSkillsValues.currDevTool} />
-            <UnorderedList itemList={libraryListItems} className="preview-skill-list" />
-            <BulletListField fieldName='Libraries' onInputChange={technicalSkillsData.technicalSkillsListeners.handleLibraryChange} onAddChange={technicalSkillsData.technicalSkillsListeners.handleAddCurrLibrary} value={technicalSkillsData.technicalSkillsValues.currLibrary} />
+            <BulletListField fieldName='Programming Languages' onInputChange={technicalSkillsData.technicalSkillsListeners.handleLanguageChange} onAddChange={technicalSkillsData.technicalSkillsListeners.handleAddCurrLanguage} value={technicalSkillsData.technicalSkillsValues.currLanguage} classIdentifier={"unordered-profile-list"} itemList={languageListItems}/>
+            <BulletListField fieldName='Frameworks' onInputChange={technicalSkillsData.technicalSkillsListeners.handleFrameworkChange} onAddChange={technicalSkillsData.technicalSkillsListeners.handleAddCurrFramework} value={technicalSkillsData.technicalSkillsValues.currFramework} classIdentifier={"unordered-profile-list"} itemList={frameworkListItems}/>
+            <BulletListField fieldName='Developer Tools' onInputChange={technicalSkillsData.technicalSkillsListeners.handleDevToolChange} onAddChange={technicalSkillsData.technicalSkillsListeners.handleAddCurrDevTool} value={technicalSkillsData.technicalSkillsValues.currDevTool} classIdentifier={"unordered-profile-list"} itemList={devToolListItems}/>
+            <BulletListField fieldName='Libraries' onInputChange={technicalSkillsData.technicalSkillsListeners.handleLibraryChange} onAddChange={technicalSkillsData.technicalSkillsListeners.handleAddCurrLibrary} value={technicalSkillsData.technicalSkillsValues.currLibrary} classIdentifier={"unordered-profile-list"} itemList={libraryListItems}/>
         
         </div>
     )
