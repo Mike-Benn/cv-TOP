df08242afdebc751b00b91214910605dc5f75b01
Mike-Benn <mbenn49@wgu.edu>
2024-07-09 02:51:01 -0400

    Delete comments and restructure personal info

--- src/components/form-components/EducationInfoForm.jsx
+++ src/components/form-components/EducationInfoForm.jsx
@@ -8,79 +8,7 @@ import PropTypes from 'prop-types';
 
 
 function EducationInfoForm({ educationInfoData }) {
-/*
-    const [currOrganization , setCurrOrganization] = useState("");
-    const [currProgram , setCurrProgram] = useState("");
-    const [currEnrolled, setCurrEnrolled] = useState(false);
-    const [currGPA , setCurrGPA] = useState("");
-    const [currEducationProfileList , setCurrEducationProfileList] = useState([]);
-    
 
-    const handleOrganizationChange = (e) => {
-        setCurrOrganization(e.target.value);
-    }
-
-    const handleProgramChange = (e) => {
-        setCurrProgram(e.target.value);
-    }
-
-    const handleStillEnrolled = () => {
-        if (currEnrolled) {
-            setCurrEnrolled(false);
-        } else {
-            setCurrEnrolled(true);
-        }
-    }
-
-    const handleGPAChange = (e) => {
-        setCurrGPA(e.target.value);
-    }
-
-    const handleSubmitEducationProfile = () => {
-        if (currEducationProfileList.length <= 3) {
-            let profile = {
-                id: uuidv4(),
-                organization: currOrganization,
-                program: currProgram,
-                stillEnrolled: currEnrolled,
-                gpa: currGPA
-            }
-            let updatedProfileList = currEducationProfileList.concat([profile]);
-            setCurrEducationProfileList(updatedProfileList);
-            resetForm();
-        } else {
-            alert("Please submit no more than three of your most relevant education examples whether they be collegiate degrees or some other form of formal education.");
-        }
-    }
-
-    const handleDeleteEducationProfile = (id) => {
-        let updatedProfList = currEducationProfileList.filter(item =>
-            id !== item.id
-        );
-        setCurrEducationProfileList(updatedProfList);
-    }
-    
-    const handleEditEducationProfile = (id) => {
-        let profileObject = getItemWithID(id , currEducationProfileList);
-        let profile = profileObject.profile;
-        let educationProfileList = profileObject.arr;
-
-        setCurrOrganization(profile.organization);
-        setCurrProgram(profile.program);
-        setCurrEnrolled(profile.stillEnrolled);
-        setCurrGPA(profile.gpa);
-        setCurrEducationProfileList(educationProfileList);
-
-    }
-    
-    const resetForm = () => {
-        setCurrOrganization("");
-        setCurrProgram("");
-        setCurrEnrolled(false);
-        setCurrGPA("");
-
-    }
-*/
     const educationProfileListItems = educationInfoData.educationInfoValues.currEducationProfileList.map(item =>
         <EducationProfileListItem key={item.id} data={item} onDelete={educationInfoData.educationInfoListeners.handleDeleteEducationProfile} onEdit={educationInfoData.educationInfoListeners.handleEditEducationProfile} />
     )
@@ -89,7 +17,7 @@ function EducationInfoForm({ educationInfoData }) {
     return (
         <div className='form-section'>
             <h2 className='form-header'>Education</h2>
-            <UnorderedList itemList={educationProfileListItems} className="education-experience-list" />
+            <UnorderedList itemList={educationProfileListItems} className="unordered-profile-list" />
             <SmallTextField fieldName='University/Organization' placeHolder='University of Illinois' onInputChange={educationInfoData.educationInfoListeners.handleOrganizationChange} value={educationInfoData.educationInfoValues.currOrganization}/>
             <SmallTextField fieldName='Program/Degree' placeHolder='BS in Computer Science' onInputChange={educationInfoData.educationInfoListeners.handleProgramChange} value={educationInfoData.educationInfoValues.currProgram}/>
             <CheckboxField fieldName='Still Enrolled' onInputChange={educationInfoData.educationInfoListeners.handleStillEnrolled} checked={educationInfoData.educationInfoValues.currEnrolled} value={educationInfoData.educationInfoValues.currEnrolled}/>
--- src/components/form-components/PersonalInfoForm.jsx
+++ src/components/form-components/PersonalInfoForm.jsx
@@ -1,44 +1,27 @@
 
-import TextArea from './TextArea';
+import SmallEmailField from './SmallEmailField';
 import SmallTextField from './SmallTextField';
 import PropTypes from 'prop-types'
+import SmallPhoneNumberField from './SmallPhoneNumberField';
+import SmallUrlField from './SmallUrlField';
 
-function PersonalInfoForm({ personalInfoData }) {
-/*
-    const [currFirstName , setCurrFirstName] = useState("");
-    const [currLastName , setCurrLastName] = useState("");
-    const [currTitle , setCurrTitle] = useState("");
-    const [currSummary , setCurrSummary] = useState("");
-
-    const handleFirstNameChange = (e) => {
-        setCurrFirstName(e.target.value);
-    }
-
-    const handleLastNameChange = (e) => {
-        setCurrLastName(e.target.value);
-    }
-
-    const handleTitleChange = (e) => {
-        setCurrTitle(e.target.value);
-    }
-
-    const handleSummaryChange = (e) => {
-        setCurrSummary(e.target.value);
-    }*/
+function PersonalInfoForm({ personalInfoData , contactInfoData }) {
 
     return (
         <div className='form-section'>
             <h2 className='form-header'>Personal Info</h2>
             <SmallTextField fieldName='First Name' onInputChange={personalInfoData.personalInfoListeners.handleFirstNameChange} value={personalInfoData.personalInfoValues.currFirstName}/>
             <SmallTextField fieldName='Last Name' onInputChange={personalInfoData.personalInfoListeners.handleLastNameChange} value={personalInfoData.personalInfoValues.currLastName}/>
-            <SmallTextField fieldName='Professional title' onInputChange={personalInfoData.personalInfoListeners.handleTitleChange} value={personalInfoData.personalInfoValues.currTitle}/>
-            <TextArea fieldName='Give a short summary about yourself' onInputChange={personalInfoData.personalInfoListeners.handleSummaryChange} value={personalInfoData.personalInfoValues.currSummary}/>
+            <SmallPhoneNumberField fieldName='Phone Number' onInputChange={contactInfoData.contactInfoListeners.handlePhoneNumberChange} value={contactInfoData.contactInfoValues.currPhoneNumber} />
+            <SmallEmailField fieldName='Email' onInputChange={contactInfoData.contactInfoListeners.handleEmailChange} value={contactInfoData.contactInfoValues.currEmail}/>
+            <SmallUrlField fieldName='Website/LinkedIn' onInputChange={contactInfoData.contactInfoListeners.handleUrlChange} value={contactInfoData.contactInfoValues.currUrl}/>
         </div>
     )
 }
 
 PersonalInfoForm.propTypes = {
     personalInfoData: PropTypes.object,
+    contactInfoData: PropTypes.object,
     
 
 }
