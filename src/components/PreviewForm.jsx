9124d982d01c6ecb03655a62f2267a824d9cb73a
Mike-Benn <mbenn49@wgu.edu>
2024-07-09 05:33:22 -0400

    Add print button to test printing form

--- src/components/PreviewForm.jsx
+++ src/components/PreviewForm.jsx
@@ -4,18 +4,30 @@ import PersonalInfoPreview from "./preview-components/PersonalInfoPreview";
 import TechnicalSkillsPreview from "./preview-components/TechnicalSkillsPreview";
 import ProjectsPreview from "./preview-components/ProjectsPreview";
 import PropTypes from 'prop-types';
+import { useRef } from "react";
+import { useReactToPrint } from "react-to-print";
+
 
 function PreviewForm({ personalInfoValues , contactInfoValues , educationInfoValues , workExperienceValues , projectsValues , technicalSkillsValues}) {
+    
+    const componentRef = useRef();
+    const handlePrint = useReactToPrint({
+        content: () => componentRef.current,
+    });
+    
     return (
         <section className="preview">
-            <div className="preview-container">
+            <button type="button" onClick={handlePrint}></button>
+            <div className="preview-container" ref={componentRef}>
                 <PersonalInfoPreview personalInfoValues={personalInfoValues} contactInfoValues={contactInfoValues} />
                 <EducationInfoPreview educationInfoValues={educationInfoValues} />
                 <WorkExperiencePreview workExperienceValues={workExperienceValues} />
                 <ProjectsPreview projectsValues={projectsValues} />
                 <TechnicalSkillsPreview technicalSkillsValues={technicalSkillsValues} />
-
+                
             </div>
+            
+            
         </section>
     )
 
