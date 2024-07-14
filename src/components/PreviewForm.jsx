e8c096ef47f09a38c690d24b181208af26388e56
Mike-Benn <mbenn49@wgu.edu>
2024-07-14 02:46:59 -0400

    Add buttons for print and switch display

--- src/components/PreviewForm.jsx
+++ src/components/PreviewForm.jsx
@@ -4,6 +4,8 @@ import PersonalInfoPreview from "./preview-components/PersonalInfoPreview";
 import TechnicalSkillsPreview from "./preview-components/TechnicalSkillsPreview";
 import ProjectsPreview from "./preview-components/ProjectsPreview";
 import PropTypes from 'prop-types';
+import eye from '../images/eye.png';
+import printer from '../images/printer.png';
 import { useRef } from "react";
 import { useReactToPrint } from "react-to-print";
 
@@ -17,14 +19,16 @@ function PreviewForm({ personalInfoValues , contactInfoValues , educationInfoVal
     
     return (
         <section className="preview">
-            <button type="button" onClick={handlePrint}></button>
+            <div className="button-overlay">
+                <button type="button" className="toggle-display-button"><img src={eye} alt="Open eye ball icon" className="overlay-icon" /></button>
+                <button type="button" onClick={handlePrint} className="print-button"><img src={printer} alt="Printer icon" className="overlay-icon" /></button>
+            </div>
             <div className="preview-container" ref={componentRef}>
                 <PersonalInfoPreview personalInfoValues={personalInfoValues} contactInfoValues={contactInfoValues} />
                 <EducationInfoPreview educationInfoValues={educationInfoValues} />
                 <WorkExperiencePreview workExperienceValues={workExperienceValues} />
                 <ProjectsPreview projectsValues={projectsValues} />
                 <TechnicalSkillsPreview technicalSkillsValues={technicalSkillsValues} />
-                
             </div>
             
             
