659f3096f4f1769689ae8b484b896dde0467b321
Mike-Benn <mbenn49@wgu.edu>
2024-07-15 03:19:51 -0400

    Add variable and parsing function Const variable that is an object containing the months of the year Parsing function that changes string from month input to more readable version

--- src/utils/utils.jsx
+++ src/utils/utils.jsx
@@ -1,4 +1,19 @@
-export { getItemWithID , createStringFromList }
+export { getItemWithID , createStringFromList , parseDateString }
+
+const MONTHS = {
+    "01": "January",
+    "02": "February",
+    "03": "March",
+    "04": "April",
+    "05": "May",
+    "06": "June",
+    "07": "July",
+    "08": "August",
+    "09": "September",
+    "10": "October",
+    "11": "November",
+    "12": "December",
+}
 
 function getItemWithID(id , arr) {
     let arrCopy = [...arr];
@@ -21,4 +36,10 @@ function createStringFromList(arr) {
         }
         result = result + arr[i].value + ", ";
     }
+}
+
+function parseDateString(str) {
+    let arr = str.split("-");
+    return `${MONTHS[arr[1]]} ${arr[0]}`
+
 }
